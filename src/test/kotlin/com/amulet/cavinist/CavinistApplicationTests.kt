e10731851e5c34a7dc08d4cbb0f4dd407aafd7fa
Antoine Mulet <ntn.mulet@gmail.com>
2020-04-10 10:46:42 +0200

    initial setup using spring initializr

---
+++ src/test/kotlin/com/amulet/cavinist/CavinistApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amulet.cavinist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CavinistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
